182a54457c831f87b655dad871f839cbe1808775
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-26 16:26:15 +0200

    Add new class Main and calculation of numbers

---
+++ src/main/java/org/example/app/Main.java
@@ -0,0 +1,12 @@
+package org.example.app;
+
+import org.example.app.controller.CalcController;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CalcController controller = new CalcController();
+        controller.getResult();
+    }
+}
